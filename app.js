f987cf3003ae5f8a5100ab032be3197c6e5a8dde
lifeherbs <158531590@qq.com>
2020-12-07 11:41:18 +0800

    初始化云开发环境，完善了数据库集合

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 App({
     //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
     onLaunch: function(options) {
-        
+        wx.cloud.init({
+            env:'jinan-vh0xu',
+            traceUser:true
+        })
     },
     onShow: function(options) {
 
