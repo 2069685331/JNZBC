75a91dddead2f5a595457b4fb72dac2253ff5fdd
榊 <2069685331@qq.com>
2020-12-17 17:29:10 +0800

    设置锚点（cateindex的statushandle与index完成同步），修改图片

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,19 +1,13 @@
+// index.js
 Page({
-  data: {
-      hiddenmodalput: true,   // 控制弹窗显示隐藏
-      textareaVal: '',        // textarea的文本值
+    data: {},
+    // 跳详情页
+    jump (event) {
+        // 获取到跳转锚点id
+        let detail = event.currentTarget.dataset.detail;
 
-  },
-
-  isfouce: function () {
-      this.setData({
-          hiddenmodalput: false
-      })
-  },
-
-  textarea: function (e) {
-      this.setData({
-          textareaVal: e.detail.dataset.value
-      })
-  }
+        wx:wx.navigateTo({
+          url: '/pages/testtest/testtest?detail=' + detail,  // 通过url传到跳转页面
+        })
+    },
 })
